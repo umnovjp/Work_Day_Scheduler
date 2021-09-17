c53f3955a325b0e8f057afc6dfd0be58a918546f
umnovjp <88174852+umnovjp@users.noreply.github.com>
2021-09-16 19:54:22 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-var hourNumber = 5;
+var hourNumber = 0;
 var AMPM;
+
 
     for (i = 0; i < 9; i++) {
 
@@ -19,23 +20,29 @@ var AMPM;
         else {hourNumber = i - 3;
             console.log(hourNumber);
             AMPM = 'PM'}
+            function createDelay() {console.log(i)};
       spanTag0.textContent =  hourNumber + AMPM;
-      spanTag0.setAttribute("class", "p-style")
+      spanTag0.setAttribute("class", "hour")
       spanTag0.setAttribute("id", "pId")
       //document.getElementById('divId').appendChild(spanTag0);
       divTag.appendChild(spanTag0);
       var spanTag1 = document.createElement("span");
       spanTag1.textContent = 'tag1 ' + i;
-      spanTag1.setAttribute("class", "p2-style")
+      spanTag1.setAttribute("class", "future")
      // pTag1.setAttribute("id", "pId")
      // document.getElementById('pId').appendChild(pTag1);
       divTag.appendChild(spanTag1);
       var spanTag2 = document.createElement("span");
       spanTag2.textContent = '🔒';
-      spanTag2.setAttribute("class", "p3-style")
+      spanTag2.setAttribute("class", "lock-style")
       spanTag2.setAttribute("id", "pId")
       //document.getElementById('divId').appendChild(spanTag0);
       divTag.appendChild(spanTag2);
     }; // end for loop
 
- 
+    setTimeout(createDelay, 5000)
+
+  //  var time = moment();
+    var date = moment().format("dddd, MMMM Do");
+    var hour = moment().format("HH");
+    $('.container').text(date); //place var date to class container
